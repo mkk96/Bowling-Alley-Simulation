bb46fe81fcf4bb2c3421ccd8fb88e0eab6c0841b
Siddhant Chandra Kulshrestha <siddhantkulshrestha27@gmail.com>
2022-02-15 19:50:43 +0530

    Removed Alley Class

--- drive.java
+++ drive.java
@@ -8,8 +8,7 @@ public class drive {
 		int numLanes = 3;
 		int maxPatronsPerParty=5;
 
-		Alley a = new Alley( numLanes );
-		ControlDesk controlDesk = a.getControlDesk();
+		ControlDesk controlDesk = new ControlDesk(numLanes);
 
 		ControlDeskView cdv = new ControlDeskView( controlDesk, maxPatronsPerParty);
 		controlDesk.subscribe( cdv );
